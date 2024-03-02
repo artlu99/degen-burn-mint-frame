740e80b36508b0c58d5c3f3273804960e39fafe6
artlu99 <alu99.sm@gmail.com>
2024-03-02 09:38:00 -0500

    billboard

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -14,7 +14,7 @@ export async function GET(req: NextRequest, res: NextResponse) {
       post_url: `${process.env.BASE_URL}/frame`,
       buttons: [{ label: "Mint NFT", action: "post" }],
       aspect_ratio: "1:1",
-      cid: "QmSYN7KT847Nado3fxFafYZgG6NXTMZwbaMvU9jhu5nPmJ",
+      cid: "QmbseWUy4GAo5GMdoeWqA827tPZmBSnrH5nFBYNtfr2M78",
     });
     return new NextResponse(frameMetadata);
   } catch (error) {
