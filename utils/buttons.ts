08b0f9aaa326f0a9a01cc0c91a845b1fa8bde3b7
artlu99 <alu99.sm@gmail.com>
2024-03-04 11:09:43 -0500

    add button

--- utils/buttons.ts
+++ utils/buttons.ts
@@ -1,14 +1,22 @@
+const contractAddress = process.env.CONTRACT_ADDRESS as string;
+
 export const redirectButtons = [
   {
     buttonId: 1,
-    label: "Pinata Tutorial",
-    url: "https://www.pinata.cloud/blog/how-to-build-a-farcaster-frame-that-mints-nfts",
-    analytics: "frame-mint-tutorial-blog",
+    label: "All holders",
+    url: `https://basescan.org/token/${contractAddress}#balances`,
+    analytics: "frame-mint-contract-holders",
   },
   {
     buttonId: 2,
-    label: "Public GH Repo",
+    label: "Frame GH Repo",
     url: "https://github.com/artlu99/degen-burn-mint-frame",
     analytics: "frame-mint-github-repo",
+  },
+  {
+    buttonId: 3,
+    label: "Pinata Tutorial",
+    url: "https://www.pinata.cloud/blog/how-to-build-a-farcaster-frame-that-mints-nfts",
+    analytics: "frame-mint-tutorial-blog",
   },
 ];
